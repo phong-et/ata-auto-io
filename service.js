9c853ab4f5cdd8aa3fba5061e7c1e569c188c1bf
Phillip <phillip7.et@gmail.com>
2020-09-18 10:17:21 +0700

    clean code

--- service.js
+++ service.js
@@ -25,44 +25,4 @@ async function sendAttendance(type) {
   log(`${response.url}: ${response.status}(${response.statusText})`);
   log(await response.text());
 }
-async function checkin() {
-  let url = 'https://atacore.azurewebsites.net/api/attendance/checkin',
-    options = {
-      method: 'post',
-      body: JSON.stringify({
-        checkInTime: '2000-01-01T00:00:00+00:00',
-        checkOutTime: '2000-01-01T00:00:00+00:00',
-        isCheckIn: true,
-        reason: '',
-      }),
-      headers: {
-        Authorization: cfg.tokenUser,
-        'Content-Type': 'application/json',
-      },
-    },
-    response = await fetch(url, options);
-  log(`${response.url}: ${response.status}(${response.statusText})`);
-  log(await response.text());
-}
-async function checkout() {
-  let url = 'https://atacore.azurewebsites.net/api/attendance/checkout',
-    options = {
-      method: 'post',
-      body: JSON.stringify({
-        checkInTime: '2000-01-01T00:00:00+00:00',
-        checkOutTime: '2000-01-01T00:00:00+00:00',
-        isCheckIn: false,
-        reason: '',
-      }),
-      headers: {
-        Authorization: cfg.tokenUser,
-        'Content-Type': 'application/json',
-      },
-    },
-    response = await fetch(url, options);
-  log(`${response.url}: ${response.status}(${response.statusText})`);
-  log(await response.text());
-}
-//checkin();
-//checkout();
 sendAttendance('checkout')
